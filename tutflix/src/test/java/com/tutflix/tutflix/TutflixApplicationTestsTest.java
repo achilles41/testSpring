05b5ae9a5fd7bfa475c5f66ecda97f646ed8b565
achilles41 <93310233+achilles41@users.noreply.github.com>
2022-12-24 15:38:31 +0100

    commit me

---
+++ tutflix/src/test/java/com/tutflix/tutflix/TutflixApplicationTestsTest.java
@@ -0,0 +1,25 @@
+package com.tutflix.tutflix;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TutflixApplicationTestsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		System.out.println("Setup before...");
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
